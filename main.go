49c9c90df1f3ee80defad6c7289bce71a11a26a6
Alterise <alterise.job@gmail.com>
2022-06-03 01:58:48 +0300

    add random prime generating

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"crypto/rand"
+	"math/big"
+)
 
 func main() {
-	fmt.Println("hi")
+	max := new(big.Int)
+	max.Exp(big.NewInt(10), big.NewInt(100), nil)
+	n, _ := rand.Int(rand.Reader, max)
+	for !n.ProbablyPrime(10) {
+		max.Exp(big.NewInt(10), big.NewInt(100), nil)
+		n, _ = rand.Int(rand.Reader, max)
+		n.Add(n, max.Exp(big.NewInt(10), big.NewInt(100), nil))
+	}
+	print(n.Text(10))
 }
