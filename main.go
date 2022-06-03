043fc2cda3c23a85424f5f773964e076f26d5fd0
Alex <alterise.job@gmail.com>
2022-06-04 01:57:47 +0300

    add 1-3 steps

--- main.go
+++ main.go
@@ -1,13 +1,36 @@
 package main
 
-import "math/big"
+import (
+	"crypto/rand"
+	"math/big"
+)
 
 func main() {
 	min := new(big.Int)
 	max := new(big.Int)
-	min.Exp(big.NewInt(10), big.NewInt(100), nil)
-	max.Exp(big.NewInt(10), big.NewInt(100), nil)
-	max.Mul(max, big.NewInt(2))
-	digit := generatePrimeBigInt(min, max)
-	print(digit.Text(10))
+	min.Exp(big.NewInt(2), big.NewInt(129), nil)
+	max.Exp(big.NewInt(2), big.NewInt(130), nil)
+
+	P := generatePrimeBigInt(min, max)
+	Q := generatePrimeBigInt(min, max)
+	//N := new(big.Int).Mul(P, Q)
+
+	mul := new(big.Int).Mul(
+		new(big.Int).Mul(new(big.Int).Sub(P, big.NewInt(1)), new(big.Int).Sub(Q, big.NewInt(1))),
+		new(big.Int).Mul(new(big.Int).Add(P, big.NewInt(1)), new(big.Int).Add(Q, big.NewInt(1))),
+	)
+
+	e := new(big.Int)
+	gcd := new(big.Int)
+	one := big.NewInt(1)
+
+	for e, _ = rand.Int(rand.Reader, mul); gcd.GCD(nil, nil, mul, e).Cmp(one) != 0; e, _ = rand.Int(rand.Reader, mul) {
+	}
+
+	println(e.Text(10))
+	//println(mul.Text(10))
+	//
+	//println(N.Text(10))
+	//println(P.Text(10))
+	//println(Q.Text(10))
 }
--- utils.go
+++ utils.go
@@ -7,7 +7,7 @@ import (
 
 func generatePrimeBigInt(min, max *big.Int) *big.Int {
 	var digit *big.Int
-	diff := max.Sub(max, min)
+	diff := new(big.Int).Sub(max, min)
 	for digit, _ = rand.Int(rand.Reader, diff); !(digit.Add(digit, min)).ProbablyPrime(10); digit, _ = rand.Int(rand.Reader, diff) {
 	}
 	return digit
